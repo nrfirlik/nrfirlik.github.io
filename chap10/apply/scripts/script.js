b0cb0c23da5ef4a2616947a98bf67d0d601c4c5a
nrfirlik <146121093+nrfirlik@users.noreply.github.com>
2024-01-09 14:25:48 -0500

    Add files via upload

---
+++ chap10/apply/scripts/script.js
@@ -0,0 +1,6 @@
+function content() {
+		var text = document.getElementById("new");
+		text.textContent = "Hi. I am a new paragraph that was created with a Javascript function.";
+		text.style.color = "#c0145f";
+		text.style.fontSize = "2em";
+}
